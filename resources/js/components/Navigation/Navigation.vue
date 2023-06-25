e5e80aa2f62a214e2940b8986752297c4dfc3214
Kurt Obando <kurtobando.developer@gmail.com>
2023-06-25 21:45:50 +0800

    add initial public pages

---
+++ resources/js/components/Navigation/Navigation.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="inline-flex">
+        <nav>
+            <a href="">Discover</a>
+            <a href="">Events</a>
+            <a href="">Members</a>
+        </nav>
+        <nav>
+            <a href="">Sign-in</a>
+            <a href="">Create an account</a>
+        </nav>
+    </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,5 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Home');
+    return Inertia::render('TheHome');
 });
