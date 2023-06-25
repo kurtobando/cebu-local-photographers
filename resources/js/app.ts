9f9856610b20deda36d4cce55f6d553a151a77de
Kurt Obando <kurtobando.developer@gmail.com>
2023-06-25 14:09:39 +0800

    import primevue css files

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,5 +1,7 @@
 import './bootstrap';
 import '../css/app.css';
+import "primevue/resources/primevue.min.css";
+import "primevue/resources/themes/tailwind-light/theme.css";
 
 import { createApp, h, DefineComponent } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
