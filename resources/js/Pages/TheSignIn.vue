ffa1098ac37c05faaa4737d1f3a4a8a0a1988ddc
Kurt Obando <kurtobando.developer@gmail.com>
2023-06-26 13:32:11 +0800

    initial commit for sign in page

--- resources/js/Pages/TheSignIn.vue
+++ resources/js/Pages/TheSignIn.vue
@@ -1,7 +1,29 @@
 <template>
-    <p>Sign In</p>
+    <section>
+        <Meta title="Sign In" />
+
+        <form class="flex flex-col gap-4 justify-center">
+            <h1 class="font-bold text-4xl text-center">Sign-in</h1>
+            <p class="leading-relaxed">Welcome back! Sign-in to your account now</p>
+            <div class="flex flex-col gap-4 text-center justify-center px-8">
+                <Button
+                    icon="pi pi-google"
+                    label="Sign-in with Google" />
+                <Link href="">
+                    <u>Create an account</u>
+                </Link>
+            </div>
+        </form>
+    </section>
 </template>
 
-<script lang="ts" setup></script>
+<script setup lang="ts">
+import { Link } from '@inertiajs/vue3';
+import Button from 'primevue/button';
+import Meta from '@/Components/Meta/Meta.vue';
+import LayoutAuth from '@/Layouts/LayoutAuth.vue';
+
+defineOptions({ layout: LayoutAuth });
+</script>
 
 <style scoped></style>
--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,18 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 import DefineOptions from 'unplugin-vue-define-options/vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
     plugins: [
+        viteStaticCopy({
+            targets: [
+                {
+                    dest: 'assets',
+                    src: 'resources/images',
+                },
+            ],
+        }),
         laravel({
             input: 'resources/js/app.ts',
             ssr: 'resources/js/ssr.ts',
