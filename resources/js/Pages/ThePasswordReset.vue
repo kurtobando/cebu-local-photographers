af37e885c3f7014af764428b128e82ea425ab3d7
kurtobando <38178537+kurtobando@users.noreply.github.com>
2023-07-02 11:40:39 +0800

    add password reset page (#14)

--- resources/js/Pages/ThePasswordReset.vue
+++ resources/js/Pages/ThePasswordReset.vue
@@ -1,21 +1,14 @@
 <template>
     <section class="w-full max-w-[24rem]">
-        <Meta title="Sign Up" />
+        <Meta title="Password Reset" />
         <form class="flex flex-col gap-2 justify-center">
-            <h1 class="font-bold text-4xl text-center -tracking-wide">Let's get started!</h1>
-            <h1 class="font-bold text-4xl text-center -tracking-wide">Create your account</h1>
+            <h1 class="font-bold text-4xl text-center -tracking-wide">Forgot password?</h1>
+            <h1 class="font-bold text-4xl text-center -tracking-wide">Reset them now!</h1>
             <div class="flex flex-col gap-4 text-center justify-center px-8 mt-8">
                 <InputText
                     placeholder="email address"
                     type="email" />
-                <InputText
-                    placeholder="password"
-                    type="password" />
-                <Button label="Sign up" />
-                <Button
-                    icon="pi pi-google"
-                    label="Sign up with Google"
-                    outlined />
+                <Button label="Send Password Reset" />
                 <Link :href="route('sign-in')">
                     <span class="text-slate-600">Have an account? Sign-in here</span>
                 </Link>
@@ -27,10 +20,10 @@
 <script lang="ts" setup>
 import { Link } from '@inertiajs/vue3';
 import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
 import Meta from '@/Components/Meta/Meta.vue';
 import LayoutAuth from '@/Layouts/LayoutAuth.vue';
 import useRoute from '@/composables/useRoute';
-import InputText from 'primevue/inputtext';
 
 const route = useRoute();
 
