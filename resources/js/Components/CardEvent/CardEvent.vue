d2fd34bbae3df4f43775bb1952214cef0086f7b5
Kurt Obando <kurtobando.developer@gmail.com>
2023-07-10 22:48:21 +0800

    fix event page ui, and limit to 3 columns only

--- resources/js/Components/CardEvent/CardEvent.vue
+++ resources/js/Components/CardEvent/CardEvent.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-col gap-2 p-8 py-12 border border-slate-200 shadow-sm rounded">
-        <h3 class="font-bold text-xl">{{ title }}</h3>
+        <h3 class="font-semibold text-xl">{{ title }}</h3>
         <p>{{ date }}, {{ time }}</p>
         <p>{{ location }}</p>
         <Button label="I’m Interested" />
