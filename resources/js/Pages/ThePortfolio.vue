0e4f44f109798b309d6673d9bfa09c22fb91c30e
kurtobando <38178537+kurtobando@users.noreply.github.com>
2023-07-11 21:12:14 +0800

    create card portfolio component (#25)

--- resources/js/Pages/ThePortfolio.vue
+++ resources/js/Pages/ThePortfolio.vue
@@ -3,38 +3,13 @@
         <Meta title="Portfolio" />
         <template v-slot:header>
             <div class="grid place-content-center -mt-8">
-                <div
-                    class="flex flex-col md:flex-row justify-center gap-8 items-center text-center md:text-left max-w-3xl">
-                    <img
-                        alt="avatar"
-                        class="rounded-full w-52 h-52 object-cover mx-auto"
-                        src="https://images.unsplash.com/photo-1687579520892-5160c0df4b3a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=900&q=80" />
-                    <div class="flex flex-col gap-2">
-                        <h2 class="font-bold text-2xl">John Doe</h2>
-                        <p class="leading-relaxed">
-                            I’m John Doe, located in Cebu. I am a freelance photographer, send me a message if you are
-                            interested.
-                        </p>
-                        <div class="inline-flex gap-4 mt-4 justify-center md:justify-start">
-                            <a href="#">Follow</a>
-                            <a href="#">Hire Me</a>
-                        </div>
-                        <div class="inline-flex gap-4 items-center justify-center md:justify-start">
-                            <p class="inline-flex items-center gap-2">
-                                <Image />
-                                12
-                            </p>
-                            <p class="inline-flex items-center gap-2">
-                                <UserSquare2 />
-                                1k
-                            </p>
-                            <p class="inline-flex items-center gap-2">
-                                <Medal />
-                                Contributor
-                            </p>
-                        </div>
-                    </div>
-                </div>
+                <CardPortfolio
+                    about="I’m John Doe, located in Cebu. I am a freelance photographer, send me a message if you are interested."
+                    followers="1000"
+                    image-source="https://images.unsplash.com/photo-1687579520892-5160c0df4b3a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=900&q=80"
+                    name="John Doe"
+                    posts="12"
+                    role="Contributor" />
             </div>
         </template>
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 2xl:grid-cols-6 gap-4">
@@ -51,8 +26,8 @@
 import PageLayoutPublic from '@/Layouts/PageLayoutPublic.vue';
 import LayoutTheFold from '@/Layouts/LayoutTheFold.vue';
 import CardAvatarImage from '@/Components/CardAvatarImage/CardAvatarImage.vue';
+import CardPortfolio from '@/Components/CardPortfolio/CardPortfolio.vue';
 import Meta from '@/Components/Meta/Meta.vue';
-import { Image, UserSquare2, Medal } from 'lucide-vue-next';
 
 const images = [
     {
