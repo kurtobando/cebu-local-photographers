93a0b3620227e619c6fea5bf0e2fdff4635dde44
Kurt Obando <kurtobando.developer@gmail.com>
2023-07-17 22:31:26 +0800

    wip

---
+++ resources/js/Layouts/PageLayoutDashboard.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <div class="inline-flex w-full items-center justify-between gap-8 border-b p-8">
+            <Brand />
+            <nav class="font-sm inline-flex gap-8 text-sm">
+                <a href="">Discover</a>
+                <a href="">Submit a Photo</a>
+                <a href="">My Profile</a>
+            </nav>
+        </div>
+        <slot />
+    </div>
+</template>
+
+<script setup lang="ts">
+import Brand from '@/Components/Brand/Brand.vue';
+</script>
+
+<style scoped></style>
---
+++ resources/js/Pages/Dashboard/TheDashboard.vue
@@ -0,0 +1,11 @@
+<template>
+    <p>Hello World</p>
+</template>
+
+<script setup lang="ts">
+import PageLayoutDashboard from '@/Layouts/PageLayoutDashboard.vue';
+
+defineOptions({ layout: PageLayoutDashboard });
+</script>
+
+<style scoped></style>
