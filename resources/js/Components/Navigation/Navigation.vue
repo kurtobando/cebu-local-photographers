92f4f3cc2fd828b0d8912018f7a16ea956d5cb04
Kurt Obando <kurtobando.developer@gmail.com>
2023-07-17 22:31:01 +0800

    change button and nav links font size

--- resources/js/Components/Navigation/Navigation.vue
+++ resources/js/Components/Navigation/Navigation.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="hidden lg:inline-flex gap-16">
+    <div class="hidden gap-16 text-sm lg:inline-flex">
         <nav class="inline-flex items-center gap-8">
             <Link :href="route('home')">Discover</Link>
             <Link :href="route('events')">Events</Link>
@@ -8,7 +8,9 @@
         <nav class="inline-flex items-center gap-8">
             <Link :href="route('sign-in')">Sign-in</Link>
             <Link :href="route('sign-up')">
-                <Button label="Create an account" />
+                <Button
+                    label="Create an account"
+                    class="!py-[0.9rem] !text-sm" />
             </Link>
         </nav>
     </div>
