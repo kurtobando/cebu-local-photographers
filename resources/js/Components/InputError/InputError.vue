0a69897ae7bd68a6ad6e11ccb4a33255ab8d70fe
Kurt Obando <kurtobando.developer@gmail.com>
2023-07-23 22:21:10 +0800

    create input error component

---
+++ resources/js/Components/InputError/InputError.vue
@@ -0,0 +1,13 @@
+<template>
+    <p class="text-left text-sm text-red-600">{{ text }}</p>
+</template>
+
+<script setup lang="ts">
+defineProps<Props>();
+
+interface Props {
+    text: string;
+}
+</script>
+
+<style scoped></style>
