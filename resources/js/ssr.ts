99fff3b55d51d1798d5fe296749f2ade8dc39ab7
Kurt Obando <kurtobando.developer@gmail.com>
2023-08-05 18:28:14 +0800

    tweak toast component UI, and update usage in app.ts and ssr.ts

--- resources/js/ssr.ts
+++ resources/js/ssr.ts
@@ -1,11 +1,13 @@
-import { createSSRApp, h, DefineComponent } from 'vue';
-import { renderToString } from '@vue/server-renderer';
 import { createInertiaApp } from '@inertiajs/vue3';
 import createServer from '@inertiajs/vue3/server';
+import { renderToString } from '@vue/server-renderer';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import PrimeVue from 'primevue/config';
+import Toast from 'primevue/toast';
+import ToastService from 'primevue/toastservice';
+import { createSSRApp, DefineComponent, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 import { Ziggy } from './ziggy';
-import PrimeVue from 'primevue/config';
 
 const APP_NAME = import.meta.env.VITE_APP_NAME || 'Laravel';
 const INERTIA_SSR_PORT = import.meta.env.VITE_INERTIA_SSR_PORT || 13714;
@@ -14,14 +16,16 @@ createServer((page) => {
     return createInertiaApp({
         page,
         render: renderToString,
-        title: (title) => `${title} - ${APP_NAME}`,
         resolve: (name) =>
             resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob<DefineComponent>('./Pages/**/*.vue')),
-        setup({ App, props, plugin }) {
+        setup({ App, plugin, props }) {
             return createSSRApp({ render: () => h(App, props) })
+                .component('Toast', Toast)
                 .use(plugin)
                 .use(PrimeVue)
+                .use(ToastService)
                 .use(ZiggyVue, Ziggy);
         },
+        title: (title) => `${title} - ${APP_NAME}`,
     });
 }, INERTIA_SSR_PORT);
