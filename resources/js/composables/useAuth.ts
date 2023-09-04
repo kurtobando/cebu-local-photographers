46c5d4fc46574f473090b576aa12b462efa109b0
Kurt Obando <kurtobando.developer@gmail.com>
2023-09-04 23:01:41 +0800

    add user object to useAuth composables

--- resources/js/composables/useAuth.ts
+++ resources/js/composables/useAuth.ts
@@ -1,12 +1,14 @@
-import { computed } from 'vue';
 import { usePage } from '@inertiajs/vue3';
+import { computed } from 'vue';
 import { SharedProps } from '@/types';
 
 function useAuth() {
     const isAuthenticated = computed(() => usePage<SharedProps>().props.auth.user !== null);
+    const user = computed(() => usePage<SharedProps>().props.auth.user);
 
     return {
         isAuthenticated,
+        user,
     };
 }
 
