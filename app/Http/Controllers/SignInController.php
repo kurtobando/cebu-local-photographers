b7829c657f480657f324ef4e34f7e8ae5287a920
Kurt Obando <kurtobando.developer@gmail.com>
2023-09-04 22:11:46 +0800

    redirect user to dashboard if they are sign-in already

--- app/Http/Controllers/SignInController.php
+++ app/Http/Controllers/SignInController.php
@@ -8,6 +8,10 @@ class SignInController extends Controller
 {
     public function index()
     {
+        if (auth()->check()) {
+            return redirect()->route('dashboard');
+        }
+
         return inertia('TheSignIn');
     }
 
