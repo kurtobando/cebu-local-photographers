7584a6da072af95b934e68a7661b801e17889981
Kurt Obando <kurtobando.developer@gmail.com>
2024-04-04 23:56:58 +0800

    Refactor card image components

--- resources/js/components/CardImage/CardImage.vue
+++ resources/js/components/CardImage/CardImage.vue
@@ -1,43 +1,16 @@
 <template>
-    <div>
-        <img
-            :src="imageSource"
-            :alt="imageAlt"
-            class="h-[25rem] w-full rounded object-cover" />
-        <div class="inline-flex w-full items-center justify-between py-4">
-            <AvatarWithName
-                :name="author.name"
-                :image-source="author.avatar" />
-            <ReactionCount
-                :comment="reactions.comment"
-                :heart="reactions.heart"
-                :view="reactions.view" />
-        </div>
-    </div>
+    <img
+        :alt="imageAlt"
+        :src="imageSource"
+        class="h-[18rem] w-full rounded object-cover" />
 </template>
 
-<script setup lang="ts">
-import AvatarWithName from '@/components/AvatarWithName/AvatarWithName.vue';
-import ReactionCount from '@/components/ReactionCount/ReactionCount.vue';
-
+<script lang="ts" setup>
 defineProps<Props>();
 
 interface Props {
     imageSource: string;
     imageAlt: string;
-    author: Author;
-    reactions: Reaction;
-}
-
-interface Author {
-    name: string;
-    avatar: string;
-}
-
-interface Reaction {
-    comment: number;
-    heart: number;
-    view: number;
 }
 </script>
 
---
+++ resources/js/components/CardImageWithAuthor/CardImageWithAuthor.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <img
+            :src="imageSource"
+            :alt="imageAlt"
+            class="h-[25rem] w-full rounded object-cover" />
+        <div class="inline-flex w-full items-center justify-between py-4">
+            <AvatarWithName
+                :name="author.name"
+                :image-source="author.avatar" />
+            <ReactionCount
+                :comment="reactions.comment"
+                :heart="reactions.heart"
+                :view="reactions.view" />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import AvatarWithName from '@/components/AvatarWithName/AvatarWithName.vue';
+import ReactionCount from '@/components/ReactionCount/ReactionCount.vue';
+
+defineProps<Props>();
+
+interface Props {
+    imageSource: string;
+    imageAlt: string;
+    author: Author;
+    reactions: Reaction;
+}
+
+interface Author {
+    name: string;
+    avatar: string;
+}
+
+interface Reaction {
+    comment: number;
+    heart: number;
+    view: number;
+}
+</script>
+
+<style scoped></style>
