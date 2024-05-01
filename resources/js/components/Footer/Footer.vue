c5ddc9729b5c056f81fcd4b69313737105891f5e
Kurt Obando <kurtobando.developer@gmail.com>
2024-05-01 20:08:00 +0800

    initial commit Footer component

---
+++ resources/js/components/Footer/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+    <footer class="w-full p-8">
+        <div class="flex flex-col justify-center gap-4 text-sm text-slate-600 md:flex-row">
+            <p class="text-center"><a href="">Privacy Policy</a></p>
+            <p class="text-center"><a href="">Terms of Service</a></p>
+            <p class="text-center"><a href="">Sitemap</a></p>
+            <p class="text-center">© {{ new Date().getFullYear() }} All rights reserved</p>
+        </div>
+    </footer>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
